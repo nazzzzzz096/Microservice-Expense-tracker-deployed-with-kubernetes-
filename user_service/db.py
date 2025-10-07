c774ebbec7d9ef60ce3c6047e871fa23713331fd
Nazina N <99040109+nazzzzzz096@users.noreply.github.com>
2025-10-07 16:41:25 +0530

    Update db.py

--- user_service/db.py
+++ user_service/db.py
@@ -9,8 +9,8 @@ load_dotenv()
 
 MYSQL_USER = os.getenv('MYSQL_USER')
 MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
-MYSQL_HOST = os.getenv('MYSQL_HOST', 'mysql')
-MYSQL_PORT = os.getenv('MYSQL_PORT', 3306)
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_PORT = os.getenv('MYSQL_PORT')
 MYSQL_DB = os.getenv('MYSQL_DATABASE')
 
 
@@ -23,4 +23,5 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create tables if they don't exist
 from models import Base
+
 Base.metadata.create_all(bind=engine)
